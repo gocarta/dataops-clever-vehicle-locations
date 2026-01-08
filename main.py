addeb0b900a766c3939d77efc0b0de6f7ffa2827
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2026-01-07 22:24:45 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,76 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "datablob",
+#     "requests",
+#     "simple-env",
+#     "tzdata",
+# ]
+# ///
+import datablob
+from datetime import datetime
+import math
+import requests
+import simple_env as se
+from time import sleep
+import zoneinfo
+
+AWS_BUCKET_NAME = se.get("AWS_BUCKET_NAME")
+AWS_BUCKET_PATH = se.get("AWS_BUCKET_PATH")
+CLEVER_BUS_TIME_API_KEY = se.get("CLEVER_BUS_TIME_API_KEY")
+CLEVER_TIMEZONE = se.get("CLEVER_TIMEZONE")
+tzinfo = zoneinfo.ZoneInfo(CLEVER_TIMEZONE)
+
+url = "https://bustracker.gocarta.org/bustime/api/v3/getroutes"
+params = {"format": "json", "key": CLEVER_BUS_TIME_API_KEY}
+response = requests.get(url, params=params)
+data = response.json()
+rts = [route["rt"] for route in data["bustime-response"]["routes"]]
+print("[dataops-clever-vehicle-locations] got routes:", rts)
+
+for i in range(240):
+    print("[dataops-clever-vehicle-locations] sleeping 5 seconds")
+    sleep(5)
+
+    results = []
+    page_size = 10
+    for i in range(math.ceil(len(rts) / page_size)):
+        url = "https://bustracker.gocarta.org/bustime/api/v3/getvehicles"
+        params = {
+            "format": "json",
+            "key": CLEVER_BUS_TIME_API_KEY,
+            "rt": ",".join(rts[i * page_size : (i + 1) * page_size]),
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        for bus in data["bustime-response"]["vehicle"]:
+            # 20260107 21:48
+            tmstmp = bus["tmstmp"]
+
+            dt = datetime.strptime(tmstmp, "%Y%m%d %H:%M")
+            dt = dt.replace(tzinfo=tzinfo)
+            timestamp = dt.isoformat()
+
+            results.append(
+                {
+                    "vehicle_id": bus["vid"],
+                    "route": bus["rt"],
+                    # "delayed": bus["dly"],
+                    "destination": bus["des"],
+                    # "heading": bus["hdg"],
+                    "latitude": float(bus["lat"]),
+                    "longitude": float(bus["lon"]),
+                    # "mode": bus["mode"],
+                    # "path_id": bus["pid"],
+                    # "speed": float(bus["spd"])
+                    "timestamp": timestamp,
+                }
+            )
+
+    # client for data store
+    client = datablob.DataBlobClient(
+        bucket_name=AWS_BUCKET_NAME, bucket_path=AWS_BUCKET_PATH
+    )
+
+    client.update_dataset(name="clever_vehicle_locations", version="1", data=results)
+    print(f"[dataops-clever-vehicle-locations] updated {len(results)} rows")
